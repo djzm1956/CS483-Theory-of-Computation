2cd58a3284f00f2e592a19f0a985dd0e353611c8
djzm1956 <75410121+djzm1956@users.noreply.github.com>
2022-10-13 09:24:46 -0400

    Update RegExp.java

--- RegExp.java
+++ RegExp.java
@@ -1,3 +1,12 @@
+/* This is a very simple program that I wrote using the Java Pattern class. The program tests if a string
+belongs to a language given the following regular expressions: 
+
+A1 = {w| w ∈ {0,1}*, w begins with a 1 and ends with 0}
+Regex: 1Σ*0
+
+A2 = {w| w ∈ {0,1}*, w contains at least three 1s}
+Regex: Σ* 1 Σ* 1 Σ* 1 Σ*
+*/
 import java.util.regex.Pattern;
 
 class RegExp {
